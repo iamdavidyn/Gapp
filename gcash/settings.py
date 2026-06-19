29cfffc51e6492ba812097837834c056de0017c8
iandavisyn <charlesdavison3766@gmail.com>
2026-06-19 16:09:59 +0100

    Remove STATICFILES_DIRS from settings
    Removed STATICFILES_DIRS configuration.

--- gcash/settings.py
+++ gcash/settings.py
@@ -118,11 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 # Default primary key field type
