3c04c014aa45f05ed3ce669429a6ae7f3ff9605a
iandavisyn <charlesdavison3766@gmail.com>
2026-06-19 16:42:50 +0100

    Update settings.py

--- gcash/settings.py
+++ gcash/settings.py
@@ -112,6 +112,11 @@ USE_I18N = True
 
 USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL 'static/'
+STATIC_ROOT = BASE_DIR / '/staticfiles/'
+
+
 STATICFILES_DIRS = [
     BASE_DIR / 'Gapp' / 'static/',
 ]
@@ -120,8 +125,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-STATIC_URL 'static/'
-STATIC_ROOT = BASE_DIR / '/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
